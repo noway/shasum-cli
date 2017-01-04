0e773387c6a27afb2e18b1db6542684964188a90
Way, No <noway@2ch.hk>
2017-01-04 21:20:07 +1300

    implemented missing options

--- index.js
+++ index.js
@@ -15,10 +15,16 @@ program
   .option('-b, --binary', 'read in binary mode')
   .option('-c, --check', 'read SHA384 sums from the FILEs and check them')
   .option('--tag', 'create a BSD-style checksum')
-  .option('-t, --text ', 'read in text mode (default)', {isDefault: true})
+  .option('-t, --text ', 'read in text mode (default)')
 
   .option('--base64', 'Output hash in base64')
   .option('-s, --sri', 'SRI output format for hash (sha384- prefix and base64)')
+
+  .option('--ignore-missing', 'don\'t fail or report status for missing files')
+  .option('--quiet', 'don\'t print OK for each successfully verified file')
+  .option('--status', 'don\'t output anything, status code shows success')
+  .option('--strict', 'exit non-zero for improperly formatted checksum lines')
+  .option('-w, --warn', 'warn about improperly formatted checksum lines')
 
   .parse(process.argv);
 
@@ -44,13 +50,14 @@ function hashFile(filename, data, params) {
 }
 
 function checkFromFile(filename, data, params) {
-  var lines = data.split("\n").filter(function(e){return e}); 
+  var lines = data.split("\n").filter(function(e){ return e }); 
   var bad_count = params.bad_count || 0;
+
   if (!lines.length) {
     if (bad_count > 0) {
       console.log(program.name()+": WARNING: "+bad_count+" computed checksum did NOT match");
+      process.exit(1); 
     }
-    process.exit(bad_count)
     return;
   }  
 
@@ -58,15 +65,20 @@ function checkFromFile(filename, data, params) {
   var match, file, hash;
 
   if (match = line.match(/^SHA384 \(([^ ]*)\) = ([^ ]*)$/)) {
-    file = match[1]
-    hash = match[2]
+    file = match[1];
+    hash = match[2];
   }
   else if (match = line.match(/^([^ ]*)  ([^ ]*)$/)) {
-    hash = match[1]
-    file = match[2]
+    hash = match[1];
+    file = match[2];
   }
   else {
-    console.log("WARN: couldn't parse line");
+    if (program.warn) {
+      console.log("WARN: couldn't parse line");
+    }
+    if (program.strict) {
+      process.exit(1);
+    }
     return checkFromFile(filename, lines.join("\n"), params);
   }
 
@@ -81,7 +93,7 @@ function checkFromFile(filename, data, params) {
 function processFile(filename, params) {
   var opts = { encoding: 'utf8' };
   if (program.isBinary) {
-    opts = undefined
+    opts = undefined;
   }
 
   return fs.readFileAsync(filename, opts).then(function (data) {
@@ -96,24 +108,37 @@ function processFile(filename, params) {
 
     if (params.isPrint) {
       if (program.tag) {
-        console.log("SHA384 ("+filename+") = "+hash)
+        console.log("SHA384 (" +filename+ ") = " +hash)
       }
       else {
         console.log(hash +"  "+ filename)
       } 
     }
+
     if (params.checkToHash) {
       if (params.checkToHash == hash) {
-        console.log(filename + ": OK")
+        if (program.quiet == undefined && program.status == undefined) {
+          console.log(filename + ": OK")
+        }
         return 0;
       }
       else {
-        console.log(filename + ": FAILED")
+        if (program.status == undefined) {
+          console.log(filename + ": FAILED")
+        }
         return 1;
       }
     }
-
+  })
+  .catch(function(err) {
+    if (err.code == 'ENOENT') {
+      if (program.ignoreMissing && params.checkToHash) {
+        return;
+      }
+    }
+    throw err;
   });
+
 }
 
 function fileChain() {
@@ -130,4 +155,5 @@ function fileChain() {
     return fileChain();
   });
 }
+
 fileChain();
